df622edd7fb2c09fba9935f87777a54c1fa0c4b9
Italo Fernandes <italojafernandes@hotmail.com>
2025-01-13 10:21:07 -0300

    🧑‍💻 Emitindo o id do socket para o evento 'nova mensagem'

--- server/socket/index.js
+++ server/socket/index.js
@@ -3,25 +3,26 @@ import UserController from '../controllers/userController.js';
 
 io.on('connection', (socket) => {
   const userController = UserController();
+  io.emit('nova mensagem', socket.id);
 
   socket.on('disconnect', () => {
     io.emit('user disconnected');
     const id = null;
-    
+
     userController.logoffUser(id, () => {
       io.emit('user-logoff', id);
     });
-  })
+  });
 
   socket.on('logoff', (id) => {
     userController.logoffUser(id, () => {
       io.emit('user-logoff', id);
     });
-  })
+  });
 
   socket.on('add-user-id', (id) => {
     userController.logonUser(id, socket, () => {
       io.emit('user-logged', id);
     });
-  })
-})
+  });
+});
